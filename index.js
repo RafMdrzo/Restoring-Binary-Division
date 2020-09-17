a7e57d55f538c69ef4ddb9eb28d1e654ab2caef2
Rafael Maderazo <rafaelmaderazo@gmail.com>
2020-09-17 18:19:13 +0800

    added other init files for node

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //placeholder file
+const express = require('express');
+const port = process.env.PORT || 8000;
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
+
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res)
+{
+       res.render("home", {});
+});
+
+
+app.listen(port, function()
+{
+    console.log('listening at port ' + port);
+});

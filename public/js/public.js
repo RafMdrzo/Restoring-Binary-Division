c53e4ecc805e00f00b4ddc4795a76458cee75c7f
Rafael Maderazo <rafaelmaderazo@gmail.com>
2020-09-20 16:13:21 +0800

    getting input values now possible

--- public/js/public.js
+++ public/js/public.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
 
     $("#divide-sbs").click(function(){
         console.log("hello")
+        var dividend = $("#dividend").val();
+        var divisor = $("#divisor").val();
+        
         $("#divide-sbs").html('Next');
     });
-
+    $("#divide-skip").click(function(){
+        console.log("hello")
+        var dividend = $("#dividend").val();
+        var divisor = $("#divisor").val();
+        
+    });
 });

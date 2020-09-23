d6db78fae91e21ee9475dc9e027c8870389287f5
Eugenio Pastoral <stonenobrein@gmail.com>
2020-09-23 23:37:49 +0800

    Added rendering of division solution

--- public/js/public.js
+++ public/js/public.js
@@ -4,13 +4,19 @@ $(document).ready(function(){
         console.log("hello")
         var dividend = $("#dividend").val();
         var divisor = $("#divisor").val();
-        
+
+        $.get('/divide', {dividend: dividend, divisor: divisor}, function(result){
+          $("#SBS-solution-box").append(
+            '<pre>' + result + '</pre>');
+        });
+
         $("#divide-sbs").html('Next');
     });
+
     $("#divide-skip").click(function(){
         console.log("hello")
         var dividend = $("#dividend").val();
         var divisor = $("#divisor").val();
-        
+        console.log(divisor);
     });
 });

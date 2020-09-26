42c731df52e003a2ab4b4654742c576295ec36b6
Rafael Maderazo <rafaelmaderazo@gmail.com>
2020-09-26 16:32:57 +0800

    made result to array in front-end. iteration is pending

--- public/js/public.js
+++ public/js/public.js
@@ -1,16 +1,60 @@
+
+function filter_array(test_array) {
+    var index = -1,
+        arr_length = test_array ? test_array.length : 0,
+        resIndex = -1,
+        result = [];
+
+    while (++index < arr_length) {
+        var value = test_array[index];
+
+        if (value) {
+            result[++resIndex] = value;
+        }
+    }
+
+    return result;
+}
+
 $(document).ready(function(){
+    var first_pass = true
+    var iter = 0
+    var filtered = []
 
     $("#divide-sbs").click(function(){
-        console.log("hello")
         var dividend = $("#dividend").val();
         var divisor = $("#divisor").val();
+        
+        
 
-        $.get('/divide', {dividend: dividend, divisor: divisor}, function(result){
-          $("#SBS-solution-box").append(
-            '<pre>' + result + '</pre>');
-        });
+        if(first_pass == true){
+            var resd = $.ajax({
+                type: 'GET',       
+                url: "/divide",
+                data: {dividend: dividend, divisor: divisor} ,
+                dataType: 'text',
+                context: document.body,
+                global: false,
+                async:false,
+                success: function(data) {
+                    return data;
+                }
+            }).responseText;
 
+            resd = resd.split("\n");
+            filtered = filter_array(resd);
+
+            $("#SBS-solution-box").append(
+                "<pre>" + filtered[0] + "<br><br>" + "</pre>");
+                first_pass = false;
+                
+            iter = 1
+        } else {
+            console.log(iter)
+        }
+        
         $("#divide-sbs").html('Next');
+        //change the element id of step-by-step to next-step
     });
 
     $("#divide-skip").click(function(){
